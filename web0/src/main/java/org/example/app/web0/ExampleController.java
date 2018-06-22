2dd73df1a64289d0a66b69a7d85de8b025b164ba
Duane Zamrok <2879394+dewthefifth@users.noreply.github.com>
2018-06-22 11:13:34 -0400

    Update the root project to use spring-boot as a parent Updated web1 and web2 to use Tomcat, hoping it would fix the exceptions being thrown by JBOSS when they're initialized (effectively made the match web0), but it didn't work.

--- web0/src/main/java/org/example/app/web0/ExampleController.java
+++ web0/src/main/java/org/example/app/web0/ExampleController.java
@@ -1,6 +1,7 @@
 package org.example.app.web0;
 
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -11,7 +12,7 @@ public class ExampleController {
     @Value("${welcome.message}")
     private String welcomeMessage;
 
-    @RequestMapping("")
+    @GetMapping("")
     public String index() {
         return welcomeMessage;
     }
